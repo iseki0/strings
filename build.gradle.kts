160a3bcca83a9ddd8410ed36abe134a30b2166ef
iseki <admin@iseki.space>
2023-07-28 11:26:04 +0800

    build: add jacoco

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     kotlin("jvm") version "1.9.0"
     id("me.champeau.jmh") version "0.7.1"
+    jacoco
 }
 
 group = "space.iseki.strings"
